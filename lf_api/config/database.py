b97d5ffe1d576b87b0d6cdefb6d5a4d6713dffe9
Luiz Antonio Calliari Filho <luzcalliari.filho@protonmail.com>
2023-06-04 23:37:48 -0300

    Adding environment variables.

--- lf_api/config/database.py
+++ lf_api/config/database.py
@@ -1,16 +1,18 @@
-from sqlalchemy import create_engine
+import os
+
+from sqlalchemy import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_TYPE = "postgresql"
-DB_LOGIN = "postgres"
-DB_PASS = "postgres"
-DB_HOST = "lf_psql"
-DB_NAME = "lf000"
+DB_TYPE = os.environ.get("DB_TYPE")
+DB_LOGIN = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_TABLE = os.environ.get("DB_TABLE")
 
-SQLALCHEMY_DATABASE_URL = f"{DB_TYPE}://{DB_LOGIN}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
+SQLALCHEMY_DATABASE_URL = f"{DB_TYPE}://{DB_LOGIN}:{DB_PASS}@{DB_HOST}/{DB_TABLE}"
 
-engine = create_engine(
+engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
