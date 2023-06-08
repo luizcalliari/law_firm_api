77cfdd96da3d9934847698760b8c1836eec1abc4
Luiz Antonio Calliari Filho <luzcalliari.filho@protonmail.com>
2023-06-07 22:11:30 -0300

    Creating a foreightkey between client and client_status table.

--- lf_api/client/model.py
+++ lf_api/client/model.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalachemy.orm import relationship
 
 from lf_api.config.database import DbBase
 
@@ -16,7 +17,7 @@ class Client(DbBase):
     observation = Column(String)
     cpf = Column(String, unique=True)
     cnpj = Column(String, unique=True)
-    status_id = Column(Integer, default=1, nullable=False)
+    status_id = Column(Integer, default=1, nullable=False, ForeignKey='client_status.id')
 
 
 class ClientStatus(DbBase):
@@ -25,3 +26,4 @@ class ClientStatus(DbBase):
     seq = DbBase.Sequence('client_id_seq')
     id = Column(Integer, seq, server_default=seq.next_value(), primary_key=True, index=True, nullable=False)
     description = Column(String, unique=True, nullable=False)
+    clients = relationship('Client', back_populates = 'client_status')
