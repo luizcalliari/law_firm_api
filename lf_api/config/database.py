8bc0a2431af77b0cbedddf753d1e4c0026b9d207
Luiz Antonio Calliari Filho <luzcalliari.filho@protonmail.com>
2023-06-12 22:14:41 -0300

    Adding basic db session configuration.

--- lf_api/config/database.py
+++ lf_api/config/database.py
@@ -17,3 +17,12 @@ engine = create_async_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 DbBase = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
