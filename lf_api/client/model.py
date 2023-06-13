e452dc86c9c5d5f824244017c236b9a687e97a51
Luiz Antonio Calliari Filho <luzcalliari.filho@protonmail.com>
2023-06-12 22:13:57 -0300

    Fixing clients models.

--- lf_api/client/model.py
+++ lf_api/client/model.py
@@ -1,15 +1,14 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, DefaultClause, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY
-from sqlalachemy.orm import relationship
+from sqlalchemy.orm import relationship
 
-from lf_api.config.database import DbBase
+from config.database import DbBase
 
 
 class Client(DbBase):
     __tablename__ = "client"
 
-    seq = DbBase.Sequence('client_id_seq')
-    id = Column(Integer, seq, server_default=seq.next_value(), primary_key=True, index=True, nullable=False)
+    id = Column(Integer, server_default=DefaultClause("nextval('client_id_seq'::regclass)"), primary_key=True, index=True, nullable=False)
     name = Column(String, unique=True, nullable=False)
     addresses = Column(ARRAY(String))
     phone = Column(ARRAY(String))
@@ -17,13 +16,12 @@ class Client(DbBase):
     observation = Column(String)
     cpf = Column(String, unique=True)
     cnpj = Column(String, unique=True)
-    status_id = Column(Integer, default=1, nullable=False, ForeignKey='client_status.id')
+    status_id = Column(Integer, ForeignKey('client_status.id'), default=1, nullable=False)
 
 
 class ClientStatus(DbBase):
     __tablename__ = "client_status"
 
-    seq = DbBase.Sequence('client_id_seq')
-    id = Column(Integer, seq, server_default=seq.next_value(), primary_key=True, index=True, nullable=False)
+    id = Column(Integer, server_default=DefaultClause("nextval('client_status_id_seq'::regclass)"), primary_key=True, index=True, nullable=False)
     description = Column(String, unique=True, nullable=False)
-    clients = relationship('Client', back_populates = 'client_status')
+    clients = relationship('Client', back_populates='client_status')
